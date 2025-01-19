a357d0d0e7e42d7abb94272fe9f0c96baecee91f
stevenennis <seotieno@safaricom.co.ke>
2025-01-19 18:45:09 +0300

    ft(add task): add addTask call back function
    - add callback function in app.js file

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 import AddTask from './components/AddTask';
 
 function App() {
+
+  const addTask = () => {};
+  
   return (
     <div className="App">
       <h1>Task Manager</h1>
-      <AddTask />
+      <AddTask addTask={addTask} />
     </div>
   );
 }
